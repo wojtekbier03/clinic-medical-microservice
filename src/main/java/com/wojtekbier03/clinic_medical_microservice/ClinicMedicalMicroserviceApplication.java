034f10cadc93e3c1934f8e6a3616a312d3648deb
wojciech.biernacki <w.biernacki03@gmail.com>
2024-07-21 02:43:24 +0200

    commit

---
+++ src/main/java/com/wojtekbier03/clinic_medical_microservice/ClinicMedicalMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.wojtekbier03.clinic_medical_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicMedicalMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicMedicalMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wojtekbier03/clinic_medical_microservice/ClinicMedicalMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wojtekbier03.clinic_medical_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicMedicalMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
